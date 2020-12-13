5283cc9bd38efc811d194703b09ef7aa58d4e91a
JosiahACT <josiah.filleul@weareact.com>
2020-12-13 12:01:53 +0000

    Some refactoring

--- src/day13/day13part1.py
+++ src/day13/day13part1.py
@@ -1,5 +1,7 @@
 """
 Day 13 Part 1
+
+The fact this was easy meant I was already gearing up for a hard part 2.
 """
 
 file_contents = []
@@ -9,29 +11,32 @@ for file_line in f:
     file_contents.append(file_line.rstrip())
 f.close()
 
+# The time I can make my earliest departure
 earliest_departure = int(file_contents[0])
+
 
 list_of_services = file_contents[1].split(',')
 list_of_running_services = []
 
+# filter out services that are running
 for service in list_of_services:
     if service.isnumeric():
         list_of_running_services.append(int(service))
 
+# Test to see which service arrives first after the earliest time.
 time = earliest_departure - 1
 a = True
 winning_service = 0
 departure_time = 0
 while a:
     time += 1
-    print('time: '+ str(time))
     for service in list_of_running_services:
         if time % service == 0:
             winning_service = service
             departure_time = time
-            print(winning_service)
-            print(departure_time-earliest_departure)
             a = False
             break
 
-print((departure_time - earliest_departure) * winning_service)
+print('Service taken: ' + str(winning_service))
+print('time of departure: ' + str(departure_time-earliest_departure))
+print('Time waiting * service number: ' + str((departure_time - earliest_departure) * winning_service))
--- src/day13/day13part2.py
+++ src/day13/day13part2.py
@@ -3,7 +3,16 @@ Day 13 Part 2
 
 This part is basically chinese remainder theorem.
 
-All service numbers are prime and thus coprime
+All service numbers are prime and thus coprime so we can apply CRT.
+
+To preform this I will have an eqn in the form
+
+a*x + time = b0*x0 + ... + bn*xn where:
+a, b0,...,bn >0 , x = all service numbers mult toegther, xi = x/ith service no.,
+time = the time of arrival of the first bus where we then get the sequential arrivals
+
+We will be able to find 'a*x + time' by computing the RHS using the fact that 'xi mod service no.[j] = 0 for all i != j'
+We can then find time using the fact a is a natural number.
 """
 
 file_contents = []
@@ -15,22 +24,21 @@ f.close()
 
 # Only care about service numbers this time
 list_of_services = file_contents[1].split(',')
+# Each service number x minutes past the time for the 1st service so will be 'time = x mod service no.'
+# We have to take into account x > service number by using the fact that the service will just arrive n times before
+# our x so 'x = n * service no. + y' with y < service no.
 list_of_running_services = []
 list_of_mods = []
-
 for i in range(len(list_of_services)):
     if list_of_services[i].isnumeric():
         list_of_running_services.append(int(list_of_services[i]))
+        # find y from above ( y = x for service no. > x )
         list_of_mods.append(i % int(list_of_services[i]))
 
-services = list_of_running_services
-
-print(services)
-
+# Now we want to find our initial coefficients for the CRT eqn (the xi stated at the top)
 list_of_coeffs = []
-
-for service in services:
-    l = services.copy()
+for service in list_of_running_services:
+    l = list_of_running_services.copy()
     l.remove(service)
     coeff_tot = 1
     for s in l:
@@ -40,34 +48,34 @@ for service in services:
 print(list_of_coeffs)
 
 
-def find_lowest_sat_mod(xmody: int, coeff: int, y: int) -> int:
-    print('finding lowest mult of: ' + str(coeff) + ' s.t. it = ' + str(xmody) + ' mod ' + str(y))
-    i = 1
-    coeff_mod_y = coeff % y
+# I had to fettle this method a bit by taking mods throughout the calculation as if i simply kept multiplying the coefficient xi by
+def find_lowest_mult_of_coeff_that_satisfies_mod_condition(mod_conditon: int, coeff: int, service_no: int) -> int:
+    print('finding lowest mult of: ' + str(coeff) + ' s.t. it = ' + str(mod_conditon) + ' mod ' + str(service_no))
+    j = 1
+    coeff_mod_y = coeff % service_no
     original_coeff = coeff
-    a = 0
     while True:
-        a = coeff % y
-        if a == xmody:
+        a = coeff % service_no
+        if a == mod_conditon:
             break
-        i += 1
-        coeff = (i % y) * coeff_mod_y
+        j += 1
+        coeff = (j % service_no) * coeff_mod_y
 
     print('found')
-    print(original_coeff * i)
-    return original_coeff * i
+    print(original_coeff * j)
+    return original_coeff * j
 
 
 out = 0
 
-for i in range(len(services)):
-    out += find_lowest_sat_mod(list_of_mods[i], list_of_coeffs[i], services[i])
+for i in range(len(list_of_running_services)):
+    out += find_lowest_mult_of_coeff_that_satisfies_mod_condition(list_of_mods[i], list_of_coeffs[i], list_of_running_services[i])
 
 print(' out = ' + str(out))
 
 x = 1
 divisor = 1
-for i in range(len(services)):
+for i in range(len(list_of_running_services)):
     divisor *= list_of_running_services[i]
 
 
