e61ee62f14e6d2825f49473a90a43150857d81bf
JosiahACT <josiah.filleul@weareact.com>
2020-12-18 12:01:58 +0000

    WIP 18 2

--- src/day18/day18part1.py
+++ src/day18/day18part1.py
@@ -2,7 +2,7 @@
 list_of_sums = []
 
 # Create a list of lists to become an array of seats.
-f = open("day17input.txt", "r")
+f = open("day18input.txt", "r")
 for file_line in f:
     list_of_sums.append(list(file_line.rstrip()))
 f.close()
@@ -11,7 +11,13 @@ def find_trailing_bracket(calc: str) -> int:
 
 
 def evaluate_calc(calc: str) -> int:
-
+    # walk along term performing calculations
+    if calc[-1].isnumeric():
+        if calc[-3] == '+':
+            return evaluate_calc(calc[:-4]) + int(calc[-1])
+        if calc[-3] == '*':
+            return evaluate_calc(calc[:-4]) * int(calc[-1])
+    # If we get to bracket then call calc on bit inside
 
 def evaluate_brackets(calc: str) -> int:
     if '(' not in calc:
@@ -20,4 +26,4 @@ def evaluate_brackets(calc: str) -> int:
     return evaluate_brackets(calc[calc.find('(') + 1:find_trailing_bracket(calc)])
 
 def evaluate_line(line: str) -> int:
-    evaluate_brackets(line)
+    evaluate_calc(line)
